67b88af72876038c418e19e3980cc261805c5647
drizzle <DrizMiz@outlook.com>
2019-12-13 23:21:36 +0800

    添加注释并增加最后一题答案

--- 最大乘积（2017期末）.cpp
+++ 最大乘积（2017期末）.cpp
@@ -59,7 +59,9 @@ vector<int> frc;
 
 void srch(int n)
 {
+	// 以n=15为例
 	int sum = 0;
+	// 第一步，因为要取不同的数，从小到大遍历数使和为n，除了最后一个数以外间隔为1
 	for (int i = 2;; ++i)
 	{
 		sum += i;
@@ -75,19 +77,24 @@ void srch(int n)
 			--i;
 		}
 	}
+	// 此时得到了2,3,4,6
+	// 第二步，最后一个数和倒数第二个的差值diff
 	int diff = frc.back() - frc[frc.size() - 2] - 1;
+	// 尽可能把diff分给前面的数
+	// 如果diff比较大，则可以给前面的数每个多1（从选数方式可以知道不可能可以给更多）
 	if ((int)frc.size() - diff < 0)
 		for (int i = 0; i < frc.size() - 1; ++i)
 		{
 			++frc[i];
 			--frc.back();
 		}
+	// 如果diff比较小，则只能给靠后面的数每个多1
 	else
-	{
-		frc.back() -= diff;
 		for (int i = 0; i < diff; ++i)
+		{
 			++frc[frc.size() - 1 - i];
-	}
+			--frc.back();
+		}
 }
 
 int main()
